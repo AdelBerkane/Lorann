37ceec3fd857756b8abd98ce02b04ce1407b2656
Adel <adeltempname@gmail.com>
2018-06-03 14:07:33 +0100

    IMobile

--- model/src/main/java/model/IElements.java
+++ model/src/main/java/model/IElements.java
@@ -1,5 +1,5 @@
 package model;
-
+import model.Position;
 
 public interface IElements {
 	
--- model/src/main/java/model/Mobile.java
+++ model/src/main/java/model/Mobile.java
@@ -1,44 +1,76 @@
 package model;
 
 import model.Direction;
+import model.IMobile;
+import model.Position;
 
 import java.util.ArrayList;
-public class Mobile extends Elements implements Imobile {
-	
+
+public abstract class Mobile extends Elements implements IMobile {
+
 	protected Direction direction = null;
 	public boolean needToMove = false;
 	protected Direction lastDirection;
 	protected boolean hit = false;
-	
+
 	/**
 	 * Gets the sprite, obstacle type and position and sets direction
+	 * 
 	 * @param sprite
-	 * sprite of the element
+	 *            sprite of the element
 	 * @param obtype
-	 * obtype of the element
+	 *            obtype of the element
 	 * @param position
-	 * position of the element
+	 *            position of the element
 	 * @param direction
-	 * direction of the element
+	 *            direction of the element
 	 */
-	
+
 	public Mobile(Sprite sprite, ObstacleType obtype, Position position, Direction direction) {
 		super(sprite, obtype, position);
 		this.direction = direction;
 	}
-	
+
+	protected void moveUp() {
+		this.getPosition().setY(this.getPosition().getY() +1);
+	}
+
+	/**
+	 * Change value of X to move a controlled character right.
+	 */
+
+	protected void moveRight() {
+		this.getPosition().setX(this.getPosition().getX() + 1);
+	}
+
+	/**
+	 * Change value of Y to move a controlled character down.
+	 */
+
+	protected void moveDown() {
+		this.getPosition().setY(this.getPosition().getY() + -1);
+	}
+
+	/**
+	 * Change value of X to move a controlled character right.
+	 */
+
+	protected void moveLeft() {
+		this.getPosition().setX(this.getPosition().getX() - 1);
+	}
+
 	/**
 	 * Sets moves for the player
 	 */
-	
+
 	public void move() {
-		switch(this.getDirection()) {
+		switch (this.getDirection()) {
 		case UP:
 			this.moveUp();
 			break;
 		case DOWN:
 			this.moveDown();
-			break
+			break;
 		case RIGHT:
 			this.moveRight();
 			break;
@@ -46,7 +78,7 @@ public class Mobile extends Elements implements Imobile {
 			this.moveLeft();
 			break;
 		case NW:
-			this.moveUP();
+			this.moveUp();
 			this.moveLeft();
 		case NE:
 			this.moveUp();
@@ -59,178 +91,159 @@ public class Mobile extends Elements implements Imobile {
 			this.moveLeft();
 		}
 	}
-	
+
 	/**
 	 * Sets hit to the value of hit
 	 */
-	
+
 	public void setHit(boolean hit) {
 		this.hit = hit;
 	}
-	
+
 	/**
 	 * Allows the use of hit in another class
 	 */
-	
+
 	public boolean getHit() {
 		return this.hit;
 	}
-	
+
 	/**
 	 * Return false for isWeapon
 	 */
-	
+
 	public boolean isWeapon() {
 		return false;
 	}
-	
+
 	/**
 	 * Change value of Y to move a controlled character up.
 	 */
-	
-	protected void moveUp() {
-		this.getPosition().setY(this.getPosition().getY()-1);
-	}
-	
-	/**
-	 * Change value of X to move a controlled character right.
-	 */
-	
-	protected void moveRight() {
-		this.getPosition().setX(this.getPosition().getX()+1);
-	}
-	
-	/**
-	 * Change value of Y to move a controlled character down.
-	 */
-	
-	protected void moveDown() {
-		this.getPosition().setY(this.getPosition().getY()-1);
-	}
-	
-	/**
-	 * Change value of X to move a controlled character right.
-	 */
-	
-	protected void moveRight() {
-		this.getPosition().setX(this.getPosition().getX()-1);
-		}
-	
+
 	/**
 	 * Overloaded method in the subclasses
 	 */
 
 	public void setSprite(int color) {
 	}
-	
+
 	/**
 	 * Return false for isEnemy.
 	 */
-	
+
 	public boolean isEnemy() {
 		return false;
 	}
-	
+
 	/**
 	 * Allows the use of direction in another class.
 	 */
-	
+
 	public Direction getDirection() {
 		return this.direction;
 	}
-	
+
 	/**
 	 * Sets direction to the direction value.
 	 */
-	
+
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
-	
+
 	/**
 	 * ALlows the use of direction in another class.
 	 */
-	
+
 	public Position getPosition() {
 		return this.position;
 	}
-	
-	
-	 public boolean getNeedToMove() {
-		 return this.needToMove;
-	 }
-	 
-	 public void setNeedToMove(boolean bool) {
-		 this.needToMove = bool;
-	 }
-	 
-	 /**
-	  * Overloaded method in subclass
-	  */
-	 
-	 public void setImage(Direction direction) {
-		 
-	 }
-	 
-	 public Direction getLastDirection() {
-		 return this.lastDirection;
-	 }
-	 
-	 public void setLastDirection(Direction direction) {
-		 this.lastDirection = direction;
-	 }
-	 
-	 /**
-	  * Set moves for NPC (non player characters).
-	  */
-	 
-	 public void animate() {
-		 switch(this.getDirection()) {
-		 case UP:
-			 this.setDirection(direction.DOWN);
-			 break;
-		 case DOWN:
-			 this.setDirection(Direction.UP);
-			 break;
-		 case LEFT:
-			 this.setDirection(Direction.RIGHT);
-			 break;
-		 case RIGHT:
-			 this.setDirection(Direction.LEFT);
-			 break;
-		 case NW:
-			 this.setDirection(Direction.SE);
-			 break;
-		 case NE:
-			 this.setDirection(Direction.SW);
-			 break;
-		 case SW:
-			 this.setDirection(Direction.NE);
-			 break;
-		 case SE:
-			 this.setDirection(Direction.NW);
-			 break;
-		 }
-	 }
-	 
-	 public boolean getWin() {
-		 return false;
-	 }
-	 
-	 /**
-	  * Overloaded method in the subclasses
-	  */
-	 
-	 public void setPrivateAttribute(int privateAttribute) {
-		 
-	 }
-	 
-	 /**
-	  * Return the value of 'O' for getPrivateAttribute
-	  */
-	 
-	 public int getPrivateAttribute() {
-		 return 0;
-	 }
+
+	public boolean getNeedToMove() {
+		return this.needToMove;
+	}
+
+	public void setNeedToMove(boolean bool) {
+		this.needToMove = bool;
+	}
+
+	/**
+	 * Overloaded method in subclass
+	 */
+
+	public void setImage(Direction direction) {
+
+	}
+
+	public Direction getLastDirection() {
+		return this.lastDirection;
+	}
+
+	public void setLastDirection(Direction direction) {
+		this.lastDirection = direction;
+	}
+
+	/**
+	 * Set moves for NPC (non player characters).
+	 */
+
+	public void animate() {
+		switch (this.getDirection()) {
+		case UP:
+			this.setDirection(Direction.DOWN);
+			break;
+		case DOWN:
+			this.setDirection(Direction.UP);
+			break;
+		case LEFT:
+			this.setDirection(Direction.RIGHT);
+			break;
+		case RIGHT:
+			this.setDirection(Direction.LEFT);
+			break;
+		case NW:
+			this.setDirection(Direction.SE);
+			break;
+		case NE:
+			this.setDirection(Direction.SW);
+			break;
+		case SW:
+			this.setDirection(Direction.NE);
+			break;
+		case SE:
+			this.setDirection(Direction.NW);
+			break;
+		}
+	}
+
+	public boolean getWin() {
+		return false;
+	}
+
+	/**
+	 * Overloaded method in the subclasses
+	 */
+
+	public void animate(Position Lorann, ArrayList<Direction> possiblePath) {
+
+	}
+
+	/**
+	 * Overloaded method in the subclasses
+	 * 
+	 * @param privateAttribute
+	 */
+
+	public void setPrivateAttribute(int privateAttribute) {
+
+	}
+
+	/**
+	 * Return the value of 'O' for getPrivateAttribute
+	 */
+
+	public int getPrivateAttribute() {
+		return 0;
+	}
 
 }
