5aac6a742d3b74b8b50bab9551091d30b8d307fa
Adel <adeltempname@gmail.com>
2018-06-04 01:38:35 +0100

    starting model(Observable)

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -1,5 +1,45 @@
 package model;
 
-public class Model {
+import java.awt.Image;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Observable;
+
+import model.Direction;
+import model.IMobile;
+import model.IModel;
+import model.Position;
+
+
+public class Model extends Observable implements IModel {
+	
+	private ArrayList<IMobile> mobiles = new ArrayList<IMobile>();
+	public ArrayList<Elements> ListElements = new ArrayList<Elements>();
+	public Elements elements[][];
+	protected int Width = 20;
+	protected int Height = 5;
+	protected int actualMap = 0;
+	protected int score = 0;
+	protected int resurection = 0;
+	protected boolean gameWin = false;
+	protected boolean gameLose = false;
+	protected Map map;
+	protected String mapLevel = "";
+	
+	/**
+	 * Sets width, height, and instanciates the array of element.
+	 * @param Width
+	 * @param Height
+	 * @throws SQLException
+	 */
+	public Model(int Width, int Height) throws SQLException{
+		this.Width = Width;
+		this.Height = Height;
+		this.elements = new Elements[this.Height][this.Width];
+		
+	}
+	
+	
+	
 
 }
